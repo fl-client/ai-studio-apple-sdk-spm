d445fd13c06b771223588659bae97b039936a8f3
TrVigneshQP <159235647+TrVigneshQP@users.noreply.github.com>
2026-01-13 16:37:21 +0530

    CBT-11911: Initial setup for SPM package (#1)
    * CBT-11911: Initial setup for SPM package
    
    * CBT-11911: Updated code owners
    
    * CBT-11911: Update python script
    
    * CBT-11911: Updated swift tool version

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AIStudioAppleSDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "QPShortsPlayer",
+            targets: ["ShortsPlayer"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ShortsPlayer",
+            url: "https://firstlight.jfrog.io/artifactory/ai-studio-apple-sdk-swift-local/Release/ShortsPlayer/ShortsPlayer-1.0.3.xcframework.zip",
+            checksum: "0286bea85666ce51c42a7f5a79872c89cc8edd380c5aba9f646e404f346bede4"
+        )
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ai-studio-apple-sdk-spm
+
+
+### Setup
+To Configure Swift Package Manager to resolve AIStudio libraries for iOS and tvOS
+
+#### Prerequisites
+
+Create a `.netrc` file under your home directory with your JFrog credentials. 
+ 
+    ```
+    machine firstlight.jfrog.io
+    login <USERNAME>
+    password <PASSWORD>
+    ```
+---
+
