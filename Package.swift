d4f88bc2939e8799a9d4f9d1102da19c6c976d1d
GitHub Action <action@github.com>
2026-03-27 08:00:23 +0000

    Update Package.swift to version 0.0.2

--- Package.swift
+++ Package.swift
@@ -22,12 +22,12 @@ let package = Package(
         .binaryTarget(
             name: "AIStudioCore",
             url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioCore/AIStudioCore-0.0.2.xcframework.zip",
-            checksum: "390aa69ff03d8781b08399032becd6d8e9b07e7c4b9282f1956695f651ec1d9a"
+            checksum: "52f8e7805c8ba2dc11139fe366c123b999ffe287d29336ba9366b80de4bff461"
         ),
         .binaryTarget(
             name: "AIStudioNetwork",
             url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioNetwork/AIStudioNetwork-0.0.2.xcframework.zip",
-            checksum: "8e6b06d02a000255542e4f120f1845ef0ecf7c1a2d0f575864aaaab9ca50ef7e"
+            checksum: "481edc15aedc649005debcfcf17e939f4552fddcd68e04d8794bfc599b4c7bf2"
         )
     ]
 )
