bc7dbfd752193f11b786424bf2951e88965f4f3a
GitHub Action <action@github.com>
2026-07-17 13:51:43 +0000

    Update Package.swift to version 0.0.15

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AIStudioCore",
-            url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioCore/AIStudioCore-0.0.14.xcframework.zip",
-            checksum: "ed7ec6e881be2773b33a44c0b2d45cd507cab7158896a6f5f1be1c58d47a2879"
+            url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioCore/AIStudioCore-0.0.15.xcframework.zip",
+            checksum: "d7372620ab594fe6c1427a03ce903f8ba7e742097348ea1e8e291c4432cb166b"
         ),
         .binaryTarget(
             name: "AIStudioNetwork",
-            url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioNetwork/AIStudioNetwork-0.0.14.xcframework.zip",
-            checksum: "7cb365c28c0a896a6116372076b8f2fc29ac5418e307576ca9b7bbd52ca3dee5"
+            url: "https://firstlight.jfrog.io/artifactory/ai-stud-core-apple-swift-local/Release/AIStudioNetwork/AIStudioNetwork-0.0.15.xcframework.zip",
+            checksum: "7fd63df808c942933fe5deb6761a3bf809eebf9ce89026cc7e9c366009765677"
         )
     ]
 )
